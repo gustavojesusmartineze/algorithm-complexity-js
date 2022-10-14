fb27d346010ca01001b28868e0216370379530a8
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-13 22:34:01 -0400

    add bubbleSort algorithm complexity

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,18 +1,18 @@
 /**
- * Complejidad Temporal -> O(  )
+ * Complejidad Temporal -> O( 1 + n * 2 + 1 + 1 + 1 + 1 + 1 ) -> O(n^2)
  * Complejidad Espacial -> O(  )
  * Espacio Auxiliar -> O(  )
  */
 function bubbleSort(arreglo) {
-  let longitud = arreglo.length;
-  for (let i = 0; i < longitud; i++) {
-    for (let j = 0; j < longitud; j++) {
-      if (arreglo[j] > arreglo[j + 1]) {
-        let temporal = arreglo[j];
-        arreglo[j] = arreglo[j + 1];
-        arreglo[j + 1] = temporal;
+  let longitud = arreglo.length; // Temporal = O(1)
+  for (let i = 0; i < longitud; i++) { // Temporal = O(n)
+    for (let j = 0; j < longitud; j++) { // Temporal = O(n)
+      if (arreglo[j] > arreglo[j + 1]) { // Temporal = O(1)
+        let temporal = arreglo[j]; // Temporal = O(1)
+        arreglo[j] = arreglo[j + 1]; // Temporal = O(1)
+        arreglo[j + 1] = temporal; // Temporal = O(1)
       }
     }
   }
-  return arreglo;
+  return arreglo; // Temporal = O(1)
 }
